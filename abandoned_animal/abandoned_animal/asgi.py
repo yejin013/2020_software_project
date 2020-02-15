7169035b819ca8098eb2886ed1496c0d17f8add7
yejin <yejin013@naver.com>
2020-02-15 14:35:00 +0900

    django project start

---
+++ abandoned_animal/abandoned_animal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for abandoned_animal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abandoned_animal.settings')
+
+application = get_asgi_application()
