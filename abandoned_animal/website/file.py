64c6306c38c5e4dd4c306111b5b12806e54882d7
yejin <yejin013@naver.com>
2020-03-14 11:50:43 +0900

    file.py

--- abandoned_animal/website/file.py
+++ abandoned_animal/website/file.py
@@ -1,2 +1,11 @@
-def download():
-    return 0
+from io import BytesIO
+import requests
+
+# url로부터 파일을 임시 다운로드
+def download(url):
+    response = requests.get(url)
+    binary_data = response.content
+    temp_file = BytesIO()
+    temp_file.write(binary_data)
+    temp_file.seek(0)
+    return temp_file
