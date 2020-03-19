4faf21531413345f86d9a69db8dd77b33854c8ad
ahyeon0508 <ahyeon0508@naver.com>
2020-03-19 16:17:29 +0900

    js

--- abandoned_animal/website/static/js/main.js
+++ abandoned_animal/website/static/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // document.getElementsByName("testText")[0].onkeydown = function(e){
 //     var eventCode = e.code;
 // };
@@ -9,187 +8,178 @@
 //     }
 // };
 
-function onlyNumber(event){
-    event = event || window.event;
-    var keyID = (event.which) ? event.which : event.keyCode;
-    if ( (keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 ) 
-        return;
-    else
-        return false;
+function onlyNumber(event) {
+  event = event || window.event;
+  var keyID = (event.which) ? event.which : event.keyCode;
+  if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
+    return;
+  else
+    return false;
 }
- 
+
 function removeChar(event) {
-    event = event || window.event;
-    var keyID = (event.which) ? event.which : event.keyCode;
-    if ( keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 ) 
-        return;
-    else
-        event.target.value = event.target.value.replace(/[^0-9]/g, "");
+  event = event || window.event;
+  var keyID = (event.which) ? event.which : event.keyCode;
+  if (keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
+    return;
+  else
+    event.target.value = event.target.value.replace(/[^0-9]/g, "");
 }
 
 function fn_pw_check() {
-    var pw = document.getElementById("pw").value; //비밀번호
-    var pw2 = document.getElementById("pwChk").value; // 확인 비밀번호
+  var pw = document.getElementById("pw").value; //비밀번호
+  var pw2 = document.getElementById("pwChk").value; // 확인 비밀번호
 
-    var pattern1 = /[0-9]/;
-    var pattern2 = /[a-zA-Z]/;
-    var pattern3 = /[~!@\#$%<>^&*]/;     // 원하는 특수문자 추가 제거
+  var pattern1 = /[0-9]/;
+  var pattern2 = /[a-zA-Z]/;
+  var pattern3 = /[~!@\#$%<>^&*]/;     // 원하는 특수문자 추가 제거
 
-    if(pw != pw2) {
-        alert("비밀번호가 일치하지 않습니다.");
-        return false;
-    }
+  if (pw != pw2) {
+    alert("비밀번호가 일치하지 않습니다.");
+    return false;
+  }
 
-//    if(!pattern1.test(pw)||!pattern2.test(pw)||!pattern3.test(pw)||pw.length<8||pw.length>50){
-//         alert("영문+숫자+특수기호 8자리 이상으로 구성하여야 합니다.");
-//         return false;
-//     }          
+  //    if(!pattern1.test(pw)||!pattern2.test(pw)||!pattern3.test(pw)||pw.length<8||pw.length>50){
+  //         alert("영문+숫자+특수기호 8자리 이상으로 구성하여야 합니다.");
+  //         return false;
+  //     }          
 
-    return true;
-}
-=======
-// document.getElementsByName("testText")[0].onkeydown = function(e){
-//     var eventCode = e.code;
-// };
-
-// document.getElementsByName("testText")[0].onkeyup = function(e){
-//     if(e.code == 'Backspace'){
-//         document.getElementById("test").innerHTML = "";
-//     }
-// };
-
-function onlyNumber(event){
-    event = event || window.event;
-    var keyID = (event.which) ? event.which : event.keyCode;
-    if ( (keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 ) 
-        return;
-    else
-        return false;
-}
- 
-function removeChar(event) {
-    event = event || window.event;
-    var keyID = (event.which) ? event.which : event.keyCode;
-    if ( keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39 ) 
-        return;
-    else
-        event.target.value = event.target.value.replace(/[^0-9]/g, "");
-}
-
-function fn_pw_check() {
-    var pw = document.getElementById("pw").value; //비밀번호
-    var pw2 = document.getElementById("pwChk").value; // 확인 비밀번호
-
-    var pattern1 = /[0-9]/;
-    var pattern2 = /[a-zA-Z]/;
-    var pattern3 = /[~!@\#$%<>^&*]/;     // 원하는 특수문자 추가 제거
-
-    if(pw != pw2) {
-        alert("비밀번호가 일치하지 않습니다.");
-        return false;
-    }
-
-//    if(!pattern1.test(pw)||!pattern2.test(pw)||!pattern3.test(pw)||pw.length<8||pw.length>50){
-//         alert("영문+숫자+특수기호 8자리 이상으로 구성하여야 합니다.");
-//         return false;
-//     }          
-
-    return true;
+  return true;
 }
 
 
 
 
 function initImageUpload(box) {
-    let uploadField = box.querySelector('.image-upload');
-  
-    uploadField.addEventListener('change', getFile);
-  
-    function getFile(e){
-      let file = e.currentTarget.files[0];
-      checkType(file);
-    }
-    
-    function previewImage(file){
-      let thumb = box.querySelector('.js--image-preview'),
-          reader = new FileReader();
-  
-      reader.onload = function() {
-        thumb.style.backgroundImage = 'url(' + reader.result + ')';
-      }
-      reader.readAsDataURL(file);
-      thumb.className += ' js--no-default';
-    }
-  
-    function checkType(file){
-      let imageType = /image.*/;
-      if (!file.type.match(imageType)) {
-        throw 'Datei ist kein Bild';
-      } else if (!file){
-        throw 'Kein Bild gewählt';
-      } else {
-        previewImage(file);
-      }
-    }
-    
+  let uploadField = box.querySelector('.image-upload');
+
+  uploadField.addEventListener('change', getFile);
+
+  function getFile(e) {
+    let file = e.currentTarget.files[0];
+    checkType(file);
   }
-  
-  // initialize box-scope
-  var boxes = document.querySelectorAll('.box');
-  
-  for(let i = 0; i < boxes.length; i++) {if (window.CP.shouldStopExecution(1)){break;}
-    let box = boxes[i];
-    initDropEffect(box);
-    initImageUpload(box);
+
+  function previewImage(file) {
+    let thumb = box.querySelector('.js--image-preview'),
+      reader = new FileReader();
+
+    reader.onload = function () {
+      thumb.style.backgroundImage = 'url(' + reader.result + ')';
+    }
+    reader.readAsDataURL(file);
+    thumb.className += ' js--no-default';
   }
-  window.CP.exitedLoop(1);
-  
-  
-  
-  
-  /// drop-effect
-  function initDropEffect(box){
-    let area, drop, areaWidth, areaHeight, maxDistance, dropWidth, dropHeight, x, y;
-    
-    // get clickable area for drop effect
-    area = box.querySelector('.js--image-preview');
-    area.addEventListener('click', fireRipple);
-    
-    function fireRipple(e){
-      area = e.currentTarget
-      // create drop
-      if(!drop){
-        drop = document.createElement('span');
-        drop.className = 'drop';
-        this.appendChild(drop);
-      }
-      // reset animate class
+
+  function checkType(file) {
+    let imageType = /image.*/;
+    if (!file.type.match(imageType)) {
+      throw 'Datei ist kein Bild';
+    } else if (!file) {
+      throw 'Kein Bild gewählt';
+    } else {
+      previewImage(file);
+    }
+  }
+
+}
+
+// initialize box-scope
+var boxes = document.querySelectorAll('.box');
+
+for (let i = 0; i < boxes.length; i++) {
+  if (window.CP.shouldStopExecution(1)) { break; }
+  let box = boxes[i];
+  initDropEffect(box);
+  initImageUpload(box);
+}
+window.CP.exitedLoop(1);
+
+
+
+
+/// drop-effect
+function initDropEffect(box) {
+  let area, drop, areaWidth, areaHeight, maxDistance, dropWidth, dropHeight, x, y;
+
+  // get clickable area for drop effect
+  area = box.querySelector('.js--image-preview');
+  area.addEventListener('click', fireRipple);
+
+  function fireRipple(e) {
+    area = e.currentTarget
+    // create drop
+    if (!drop) {
+      drop = document.createElement('span');
       drop.className = 'drop';
-      
-      // calculate dimensions of area (longest side)
-      areaWidth = getComputedStyle(this, null).getPropertyValue("width");
-      areaHeight = getComputedStyle(this, null).getPropertyValue("height");
-      maxDistance = Math.max(parseInt(areaWidth, 10), parseInt(areaHeight, 10));
-  
-      // set drop dimensions to fill area
-      drop.style.width = maxDistance + 'px';
-      drop.style.height = maxDistance + 'px';
-      
-      // calculate dimensions of drop
-      dropWidth = getComputedStyle(this, null).getPropertyValue("width");
-      dropHeight = getComputedStyle(this, null).getPropertyValue("height");
-      
-      // calculate relative coordinates of click
-      // logic: click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center
-      x = e.pageX - this.offsetLeft - (parseInt(dropWidth, 10)/2);
-      y = e.pageY - this.offsetTop - (parseInt(dropHeight, 10)/2) - 30;
-      
-      // position drop and animate
-      drop.style.top = y + 'px';
-      drop.style.left = x + 'px';
-      drop.className += ' animate';
-      e.stopPropagation();
-      
+      this.appendChild(drop);
+    }
+    // reset animate class
+    drop.className = 'drop';
+
+    // calculate dimensions of area (longest side)
+    areaWidth = getComputedStyle(this, null).getPropertyValue("width");
+    areaHeight = getComputedStyle(this, null).getPropertyValue("height");
+    maxDistance = Math.max(parseInt(areaWidth, 10), parseInt(areaHeight, 10));
+
+    // set drop dimensions to fill area
+    drop.style.width = maxDistance + 'px';
+    drop.style.height = maxDistance + 'px';
+
+    // calculate dimensions of drop
+    dropWidth = getComputedStyle(this, null).getPropertyValue("width");
+    dropHeight = getComputedStyle(this, null).getPropertyValue("height");
+
+    // calculate relative coordinates of click
+    // logic: click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center
+    x = e.pageX - this.offsetLeft - (parseInt(dropWidth, 10) / 2);
+    y = e.pageY - this.offsetTop - (parseInt(dropHeight, 10) / 2) - 30;
+
+    // position drop and animate
+    drop.style.top = y + 'px';
+    drop.style.left = x + 'px';
+    drop.className += ' animate';
+    e.stopPropagation();
+
+  }
+}
+
+// Byte 수 체크 제한
+function fnChkByte(obj, maxByte) {
+  var str = obj.value;
+  var str_len = str.length;
+
+
+  var rbyte = 0;
+  var rlen = 0;
+  var one_char = "";
+  var str2 = "";
+
+
+  for (var i = 0; i < str_len; i++) {
+    one_char = str.charAt(i);
+    if (escape(one_char).length > 4) {
+      rbyte += 1;                                         //한글2Byte
+    }
+    else {
+      rbyte++;                                            //영문 등 나머지 1Byte
+    }
+
+
+    if (rbyte <= maxByte) {
+      rlen = i + 1;                                          //return할 문자열 갯수
     }
   }
->>>>>>> c0daa3b47b2321eba929ee00cdc03d707d6723ab
+
+
+  if (rbyte > maxByte) {
+    // alert("한글 "+(maxByte/2)+"자 / 영문 "+maxByte+"자를 초과 입력할 수 없습니다.");
+    alert("메세지는 최대 " + maxByte + "byte를 초과할 수 없습니다.")
+    str2 = str.substr(0, rlen);                                  //문자열 자르기
+    obj.value = str2;
+    fnChkByte(obj, maxByte);
+  }
+  else {
+    document.getElementById('byteInfo').innerText = rbyte;
+  }
+}
