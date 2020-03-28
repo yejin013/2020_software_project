11c256163d27f79355741b23e55c0c9dadca7b1a
ahyeon0508 <ahyeon0508@naver.com>
2020-03-28 21:09:39 +0900

    commentModify

--- abandoned_animal/website/static/js/comment.js
+++ abandoned_animal/website/static/js/comment.js
@@ -1,8 +1,18 @@
 // $("#commentInput").val("test"); // 값셋팅
-var commentModify = document.getElementById('#commentModify');
-// console.log(commentModify.value);
+// var commentModify = document.getElementById('#commentModify');
+// // console.log(commentModify.value);
 
-function modify(){
-    document.getElementById("commentModify").value=input();
+// function modify(){
+//     document.getElementById("commentModify").value=input();
+// }
 
+var tempcomment;
+
+function commentInput(){
+    var inputComment = document.getElementById("commentContent").value;
+    tempcomment = inputComment;
+}
+
+function output(){
+    document.getElementById("output").value = tempcomment;
 }
