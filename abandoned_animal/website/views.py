cc0a4cabb95a104e91a79fe7d9046ec78df009ef
yejin <yejin013@naver.com>
2020-03-30 20:29:59 +0900

    signup

--- abandoned_animal/website/views.py
+++ abandoned_animal/website/views.py
@@ -25,9 +25,9 @@ def signup(request):
         else:
             user = User.objects.create_user(userID=userID, username=username, password = password, phone = phone, question = question, answer=answer)
             print(user.question)
-            return render(request, 'home.html', {'question':question })
+            return render(request, 'home.html')
     else:
-        return render(request, 'signup_new.html')
+        return render(request, 'signup.html')
 
 @csrf_exempt
 def login(request):
