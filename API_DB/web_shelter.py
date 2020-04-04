f0f0d18210cf590f08e30c892ca6e26fbc8bd805
ahyeon0508 <ahyeon0508@naver.com>
2020-04-05 03:43:18 +0900

    web_shelter

--- API_DB/web_shelter.py
+++ API_DB/web_shelter.py
@@ -14,32 +14,67 @@ def sqlite_append(shelterDf):
 
 # 웹 보호소 정보 데이터 파싱 후 sqlite_append 함수 불러오기
 def collect_shelter():
-    for n in range(1, 30):
 
-        raw = requests.get("http://www.animal.go.kr/portal_rnl/map/mapInfo2.jsp?s_date=&e_date=&s_upr_cd=&"
-                           "s_org_cd=&s_up_kind_cd=&s_kind_cd=&s_name=&pagecnt=" + str(n),
-                           headers={"User-Agent": "Mozilla/5.0"})
-        html = BeautifulSoup(raw.text, 'html.parser')
+    # 보호소 개수 파악
+    raw = requests.get("https://www.animal.go.kr/front/awtis/institution/institutionList.do?menuNo=1000000059",
+                       headers={"User-Agent": "Mozilla/5.0"})
+    html = BeautifulSoup(raw.text, 'html.parser')
+    total = html.select_one('div.boardList2 ul')
 
-        # 컨테이너 div.boardArea
-        table = html.find('table')
+    raw = requests.get("https://www.animal.go.kr/front/awtis/institution/institutionList.do?pageSize=" + str(total.text[2:-2]),
+                       headers={"User-Agent": "Mozilla/5.0"})
+    html = BeautifulSoup(raw.text, 'html.parser')
 
-        trs = table.find_all('tr')
+    # 컨테이너 div.boardArea
+    table = html.select('div.boardList2 table tbody tr')
 
-        for idx, tr in enumerate(trs):  # enumerate를 사용하면 해당 값의 인덱스를 알 수 있다.
-            if idx < 20 and idx % 2 == 1:
-                tds = tr.find_all('td')
-                area = tds[0].text.strip()  # 앞뒤 여백이 있어 strip()을 사용했다.
-                name = tds[1].text.strip()
-                tel = tds[2].text.strip()
-                addr = tds[3].text.strip()
+    for tr in table:
+        tds = tr.find_all('td')
+        area = tds[0].text.strip()  # 앞뒤 여백이 있어 strip()을 사용했다.
+        name = tds[1].text.strip()
+        tel = tds[2].text.strip()
+        addr = tds[3].text.strip()
 
-                # 데이터프레임으로 넣기
-                shelter = pd.DataFrame(([[area, name, tel, addr]]),
-                    columns=["area", "name", "tel", "addr"])
+        # 검색할 주소
+        location = addr
 
-                # sqlite_append 함수 호출
-                sqlite_append(shelter)
+        # Production(실제 서비스) 환경 - https 요청이 필수이고, API Key 발급(사용설정) 및 과금 설정이 반드시 필요합니다.
+        URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCAQHutKTDQok1dWeNv8jO73HcZpcyHgqo' \
+              '&sensor=false&language=ko&address={}'.format(location)
+
+        # URL로 보낸 Requst의 Response를 response 변수에 할당
+        response = requests.get(URL)
+
+        # JSON 파싱
+        data = response.json()
+
+        # lat, lon 추출
+        if(data['results'][0]['geometry']['location']['lat']):
+            lat = data['results'][0]['geometry']['location']['lat']
+            lng = data['results'][0]['geometry']['location']['lng']
+        else:
+            location = name
+
+            # Production(실제 서비스) 환경 - https 요청이 필수이고, API Key 발급(사용설정) 및 과금 설정이 반드시 필요합니다.
+            URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCAQHutKTDQok1dWeNv8jO73HcZpcyHgqo' \
+                  '&sensor=false&language=ko&address={}'.format(location)
+
+            # URL로 보낸 Requst의 Response를 response 변수에 할당
+            response = requests.get(URL)
+
+            # JSON 파싱
+            data = response.json()
+
+            lat = data['results'][0]['geometry']['location']['lat']
+            lng = data['results'][0]['geometry']['location']['lng']
+
+
+        # 데이터프레임으로 넣기
+        shelter = pd.DataFrame(([[area, name, tel, addr, lat, lng]]),
+                               columns=["area", "name", "tel", "addr", "latitude", "longitude"])
+
+        # sqlite_append 함수 호출
+        sqlite_append(shelter)
 
 
 
