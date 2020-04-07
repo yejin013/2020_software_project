6e13d44701aa437711452357fa29efdff319430a
yejin <yejin013@naver.com>
2020-04-07 10:10:24 +0900

    findboard_missboard

--- abandoned_animal/website/views.py
+++ abandoned_animal/website/views.py
@@ -57,57 +57,82 @@ def logout(request):
     return render(request, 'home.html')
 
 def homePost(request):
-    post_list = Post.objects.all()
+    posts = Post.objects.all()
     page = request.GET.get('page', 1)
 
-    paginator = Paginator(post_list, 12)
+    paginator = Paginator(posts, 12)
+    page_range = 5
     try:
         post = paginator.page(page)
+        current_block = math.ceil(int(page) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
     except PageNotAnInteger:
         post = paginator.page(1)
+        current_block = math.ceil(int(1) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
     except EmptyPage:
         post = paginator.page(paginator.num_pages)
-    return render(request, 'home.html', {'post' : post}) # 데이터 튜플로 들어감!
-
-'''    
-    try:
-        post = paginator.get_page(page)
-    except PageNotAnInteger:
-        post = paginator.page(1)
-    except EmptyPage:
-        post = paginator.page(paginator.num_pages)
-    page_range = 5
-    current_block = math.ceil(int(page)/page_range)
-    start_block = (current_block-1) * page_range
-    end_block = start_block + page_range
-    p_range = paginator.page_range[start_block:end_block]
-'''
+        current_block = math.ceil(int(paginator.num_pages) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
+    return render(request, 'home.html', {'post' : post, 'p_range':p_range}) # 데이터 튜플로 들어감!
 
 def findBoard(request):
     posts = Post.objects.filter(menu=True)
     page = request.GET.get('page', 1)
 
     paginator = Paginator(posts, 12)
+    page_range = 5
     try:
         post = paginator.page(page)
+        current_block = math.ceil(int(page) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
     except PageNotAnInteger:
         post = paginator.page(1)
+        current_block = math.ceil(int(1) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
     except EmptyPage:
         post = paginator.page(paginator.num_pages)
-    return render(request, 'findboard.html', {'post' : post})
+        current_block = math.ceil(int(paginator.num_pages) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
+    return render(request, 'findboard.html', {'post' : post, 'p_range':p_range})
 
 def missBoard(request):
     posts = Post.objects.filter(menu=False)
     page = request.GET.get('page', 1)
 
     paginator = Paginator(posts, 12)
+    page_range = 5
     try:
         post = paginator.page(page)
+        current_block = math.ceil(int(page) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
     except PageNotAnInteger:
         post = paginator.page(1)
+        current_block = math.ceil(int(1) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
     except EmptyPage:
         post = paginator.page(paginator.num_pages)
-    return render(request, 'missboard.html', {'post' : post})
+        current_block = math.ceil(int(paginator.num_pages) / page_range)
+        start_block = (current_block - 1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
+    return render(request, 'missboard.html', {'post' : post, 'p_range':p_range})
 
 def posterBoard(request):
     post = Post.objects.all()
