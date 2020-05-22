ffddad83a0c6870926d48da78dd4eac4a4075bb0
hyewon11 <hwj1104@naver.com>
2020-05-23 06:57:02 +0900

    head 충돌 수정

--- abandoned_animal/website/static/js/check.js
+++ abandoned_animal/website/static/js/check.js
@@ -2,12 +2,6 @@
 //     confirm("수정하시겠습니까?");
 // }
 
-<<<<<<< HEAD
-// function removeData(){
-//     confirm("정말로 삭제하시겠습니까?");
-// }
-
-=======
 function deleteCheck(){
   alert("게시글 권한이 없습니다.")
 }
@@ -27,7 +21,6 @@ function commentDeleteConfirm(n) {
       return false;
   }
 }
->>>>>>> ab25f280aa52591a9761bb4cc11081b07b161871
 
 function cancleForm(){
     confirm("이 페이지를 벗어나면 마지막 저장 후 수정된 내용은 저장되지 않습니다.");
@@ -41,10 +34,6 @@ $(function() {
     $("#modify").on("click", function() {
       $("divToggle").toggle();
     });
-<<<<<<< HEAD
-  });
-=======
   });
 
   
->>>>>>> ab25f280aa52591a9761bb4cc11081b07b161871
